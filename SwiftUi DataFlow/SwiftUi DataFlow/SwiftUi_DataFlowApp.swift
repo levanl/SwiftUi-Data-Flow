8de9c053090dc6948416400268e0a8d1c1f372f5
Levan Loladze <levan.loladzew@gmail.com>
2023-12-09 21:04:16 +0400

    first commit

---
+++ SwiftUi DataFlow/SwiftUi DataFlow/SwiftUi_DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_DataFlowApp.swift
+//  SwiftUi DataFlow
+//
+//  Created by Levan Loladze on 09.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
